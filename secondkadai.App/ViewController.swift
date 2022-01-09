2f03b0e12a1301a0848aaafd6ae20ef6a9f26e6b
yuichi.sawada <regaeew@yahoo.co.jp>
2022-01-09 19:45:42 +0900

    課題２

--- secondkadai.App/ViewController.swift
+++ secondkadai.App/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBAction func handle(_ sender: Any) {
+        label.text = "swift"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
